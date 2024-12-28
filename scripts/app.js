f96834616802d848ce7868f712ded4c9c77d943e
Eclipsa-dev <claudiaolivera93@gmail.com>
2024-12-28 13:01:52 -0300

    Page ArtGalery V2.0

---
+++ scripts/app.js
@@ -0,0 +1,40 @@
+console.log("Hola Calu")
+
+let calu=0
+var calu2=3
+
+let esONoEs = true
+
+/* console.log(esONoEs)
+
+console.log(calu2+calu)
+
+let foo = prompt('Type here');
+let bar = confirm('Confirm or deny');
+
+console.log(foo, bar);
+
+if (bar){
+    console.log("Confirmaste!")
+} else {
+    console.log("No confirmaste...")
+}
+
+while (calu<10){
+    console.log(calu)
+    calu+=1
+}
+
+for(let i=0;i<10;i++){
+    console.log(i*i)
+} */
+
+let dias=["lu", "ma", "mie", "ju", "vie", "sa", "dom"]
+
+console.log(dias)
+
+console.log(dias[1])
+
+for (let j=0,j<7,j++){
+    console.log("dia ", dias[j])
+}
